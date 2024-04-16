a77d38e56ed754fce53f306c64e4c496ba1265f1
Pejoska99 <biljanapejoska985@gmail.com>
2024-04-16 21:09:28 +0200

    new commit

--- academy-management/src/academy/entities/academy.entity.ts
+++ academy-management/src/academy/entities/academy.entity.ts
@@ -27,30 +27,3 @@ export class Academy {
     
 }
 
-// import { Subject } from 'src/subject/entities/subject.entity';
-// import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-
-// @Entity()
-// export class Academy {
-//   @PrimaryGeneratedColumn()
-//   id: number;
-
-//   @Column()
-//   name: string;
-
-//   @Column()
-//   description: string;
-
-//   @Column()
-//   startDate: Date
-
-//   @Column()
-//   endDate: Date;
-
-//   @Column()
-//   price: number;
-
-//   @OneToMany(() => Subject, (subject) => subject.academy)
-//   subjects: Subject[];
-// }
-
